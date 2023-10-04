56e171aef084485edc369e18855a4dd597141406
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-04 16:56:17 +0100

    Sample unqiue workouts, their target and equivalent calorie adjusted to the person's weight

--- base.py
+++ base.py
@@ -1,7 +1,30 @@
 import math
+import random
 
+
+calories = {
+    "squat": 1.8,
+    "jumping_jacks": 2.4,
+    "push_ups": 2.0,
+    "lunges": 2.3,
+    }
+
+def merge(distribution):
+    print(distribution)
+    new_distribution = []
+    for i in range(2):
+        new_distribution.append(distribution[0] + distribution[1])
+        distribution.pop(0), distribution.pop(0)
+    if len(distribution) > 0:
+        new_distribution.extend(distribution)
+    return new_distribution
 
 def find_factors(num):
+    """
+    Finds the factors of a number
+    This factor represents the number of different workouts and what the 
+    target of each workout should be
+    """
     MAX = 5
     factors = []
     for factor_i in range(2, MAX+1):
@@ -17,8 +40,20 @@ def augment(num):
         num += 1
     return num
 
+def sample_unique(values, k):
+    """Samples k unique values from a list"""
+    sampled_values = []
+    if k > len(values):
+        raise ValueError("k cannot be greater than the length of the list")
+    while len(sampled_values) < k:
+        val = random.choice(values)
+        if val not in sampled_values:
+            sampled_values.append(val)
+    return sampled_values
+
 def main():
     present_capacity = int(input("What is your current capacity? "))
+    weight = int(input("What is your weight? "))
     desired_target = int(input("What is your desired volume target? "))
     step = round(0.1 * present_capacity)
     next_capacity = present_capacity + step
@@ -29,8 +64,24 @@ def main():
     print("Your next capacity will be: ", next_capacity, end=" ")
     daily_target = augment(next_capacity / avaliable)
     print("Your daily target is: ", daily_target)
-    workout_distribution = find_factors(daily_target)
+    workout_distributions = find_factors(daily_target)
+    workout_distribution = random.choice(workout_distributions)
+    workout_distribution = [workout_distribution[1] for i in range(workout_distribution[0])]
+    # if the distribution is for 4 or 5 workouts but the expected MET value for each workout is less than 15
+    # merge to get smaller number of workouts
+    if (len(workout_distribution) >= 4) and (workout_distribution[0] < 15) :
+        workout_distribution = merge(workout_distribution)
+
     print("You can distribute your workout as follows: ", workout_distribution)
+    selected_workouts = sample_unique(list(calories.keys()), len(workout_distribution))
+    complete_info = {}
+    print("You can do the following workouts: ", selected_workouts)
+    for i in range(len(selected_workouts)):
+        complete_info[selected_workouts[i]] = {
+            "Target MET": workout_distribution[i],
+            "Calories": calories[selected_workouts[i]] * (weight/80),
+        }
+    print(complete_info)
 
 
 if __name__ == "__main__":
