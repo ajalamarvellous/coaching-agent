4c033ff995de8e11f00ace3209b5bba4fe292023
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-04 13:40:14 +0100

    Add basic structure
    Collect present capacity, target capacity and weekly availability to determine expected completion time, next capactiy and daily target

---
+++ base.py
@@ -0,0 +1,18 @@
+import math
+
+def main():
+    present_capacity = int(input("What is your current capacity? "))
+    desired_target = int(input("What is your desired volume target? "))
+    step = round(0.1 * present_capacity)
+    next_capacity = present_capacity + step
+    
+    avaliable = int(input("How many days a week are you avaliable? "))
+    expected_completion = math.ceil((desired_target - present_capacity) / step)
+    print("You will reach your target in: ", expected_completion, " weeks")
+    print("Your next capacity will be: ", next_capacity, end=" ")
+    daily_target = math.ceil(next_capacity / avaliable)
+    print("Your daily target is: ", daily_target)
+
+
+if __name__ == "__main__":
+    main()
