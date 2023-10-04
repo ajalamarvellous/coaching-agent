aadd36b5bc2044fe9dc385ca382e345b3f69ff8f
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-04 13:41:18 +0100

    Make the daily target a multiple of 5

--- base.py
+++ base.py
@@ -1,5 +1,11 @@
 import math
 
+def augment(num):
+    """Makes the values divisible by 5"""
+    while num % 5 != 0:
+        num += 1
+    return num
+
 def main():
     present_capacity = int(input("What is your current capacity? "))
     desired_target = int(input("What is your desired volume target? "))
@@ -10,7 +16,7 @@ def main():
     expected_completion = math.ceil((desired_target - present_capacity) / step)
     print("You will reach your target in: ", expected_completion, " weeks")
     print("Your next capacity will be: ", next_capacity, end=" ")
-    daily_target = math.ceil(next_capacity / avaliable)
+    daily_target = augment(next_capacity / avaliable)
     print("Your daily target is: ", daily_target)
 
 
