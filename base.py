d189e8f3a0e2f07a88bfa88a77eb1d5c3b884c80
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-04 14:13:40 +0100

    Add factors to find the distribution of works
    The factors give the number of workouts to do and what the target met(values + number) to do

--- base.py
+++ base.py
@@ -1,7 +1,18 @@
 import math
 
+
+def find_factors(num):
+    MAX = 5
+    factors = []
+    for factor_i in range(2, MAX+1):
+        if num % factor_i == 0:
+            factor_j = num / factor_i
+            factors.append((factor_i, factor_j))
+    return factors
+
 def augment(num):
     """Makes the values divisible by 5"""
+    num = round(num)
     while num % 5 != 0:
         num += 1
     return num
@@ -18,6 +29,8 @@ def main():
     print("Your next capacity will be: ", next_capacity, end=" ")
     daily_target = augment(next_capacity / avaliable)
     print("Your daily target is: ", daily_target)
+    workout_distribution = find_factors(daily_target)
+    print("You can distribute your workout as follows: ", workout_distribution)
 
 
 if __name__ == "__main__":
